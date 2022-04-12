d0ed4c0cc40808e50691754beaf65e4a07586da1
Shuto Shikama <>
2022-04-12 23:13:23 +0900

    My First Commit

---
+++ Assets/StartRouLette.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRouLette : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("AAAAA");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
